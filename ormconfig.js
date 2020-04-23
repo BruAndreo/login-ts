670d48fa8eae45bdb68f7f49241d3878e65292a1
Bruno Andreo <brunolino2026@gmail.com>
2020-04-23 19:56:50 -0300

    Criando dotenv para configuração de variaveis de ambiente

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   name: 'default',
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'logints',
-  password: 'secretpw',
-  database: 'logints',
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import app from './app';
 
-app.listen(3333, () => console.log(`Server is online at port ${3333}`));
+app.listen(process.env.PORT, () => console.log(`Server is online at port ${process.env.PORT}`));
