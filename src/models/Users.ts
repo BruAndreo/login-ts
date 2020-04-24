4d01bb34e9770e77308023b2e9f09fa2b1af4b84
Bruno Andreo <brunolino2026@gmail.com>
2020-04-23 22:24:47 -0300

    Criando model de users

---
+++ src/models/Users.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+class Users {
+
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column('number')
+  type: number;
+
+  @Column('time without time zone')
+  createdAt: Date;
+
+  @Column('time without time zone')
+  updatedAt: Date;
+}
+
+export default Users;
